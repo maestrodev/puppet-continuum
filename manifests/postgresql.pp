dbd21f02d9c40b7d74e55afb79191286889b0a4c
Etienne Pelletier <epelletier@maestrodev.com>
2012-12-06 17:15:47 -0600

    Updated for changes to the postgresql module.

--- manifests/postgresql.pp
+++ manifests/postgresql.pp
@@ -6,8 +6,19 @@ class continuum::postgresql(
   $allowed_rules) {
 
   if $version != undef {
-    class { 'postgresql::version':
-      version => '9.0',
+    yumrepo { 'postgresql-repo':
+      name     => "postgresql-${version}",
+      baseurl  => "http://yum.postgresql.org/${version}/redhat/rhel-\$releasever-\$basearch",
+      descr    => "Postgresql ${version} Yum Repo",
+      enabled  => 1,
+      gpgcheck => 0,
+      before => Class[postgresql::server],
+    }
+    class { 'postgresql::params':
+      version             => $version,
+      manage_package_repo => false,
+      package_source      => 'yum.postgresql.org',
+      before              => Class['postgresql::server'],
     }
   }
 
