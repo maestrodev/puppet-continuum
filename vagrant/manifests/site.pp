22300c17cb413d0a901bafd799b3a92c25fabda7
Carlos Sanchez <csanchez@maestrodev.com>
2013-06-05 15:32:22 +0200

    Update vagrantfile to latest vagrant

--- vagrant/manifests/site.pp
+++ vagrant/manifests/site.pp
@@ -1,17 +1,15 @@
-node 'localhost' {
-  class { continuum:
-    version => "1.4.1-maestro-3.4.3",
-    download_maven_repo => {
-      url => "http://10.0.2.2:7777/archiva/repository/releases",
-      username => "",
-      password => "",
-    },
-    shared_secret_password => "scary",
-  }
+class { continuum:
+  version => "1.4.1",
+  # download_maven_repo => {
+  #   url => "http://10.0.2.2:7777/archiva/repository/releases",
+  #   username => "",
+  #   password => "",
+  # },
+  shared_secret_password => "scary",
+}
 
-  class { continuum::master:  }
-  class { continuum::buildagent: }
-  continuum::buildagent::vhost { "continuum-buildagent-02":
-    port => 8182
-  }
+class { continuum::master:  }
+class { continuum::buildagent: }
+continuum::buildagent::vhost { "continuum-buildagent-02":
+  port => 8182
 }
